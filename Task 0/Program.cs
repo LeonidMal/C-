467ce819b16561cb13662f87e43358fffd471d58
Leonid <rockwil91@gmail.com>
2022-10-19 20:58:01 +0300

    написание программы  на вычисление квадрата числа

---
+++ Task 0/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+Например:
+4 -> 16 
+-3- -> 9 
+-7 -> 49*/
+
+Console.WriteLine("Введите число");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+int sqrNumber = number * number;
+
+Console.WriteLine("Квадрат числа " + number + " равен " + sqrNumber);
